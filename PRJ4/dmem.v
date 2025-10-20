c0b686b74402d5e99142b4aa509ab4a0702b1dc5
a2jinhee <a2jinhee@gmail.com>
2025-10-20 01:08:17 -0400

    prj4 update

--- PRJ4/dmem.v
+++ PRJ4/dmem.v
@@ -1,9 +1,9 @@
 // You need to generate this component correctly
 module dmem (
-    input [11:0] address,
-    input clock,
-    input [31:0] data,
-    input wren,
+    input  [11:0] address,
+    input         clock,
+    input  [31:0] data,
+    input         wren,
     output [31:0] q
 );
     altsyncram #(
@@ -14,17 +14,13 @@ module dmem (
         .outdata_reg_a("UNREGISTERED"),
         .init_file("dmem.mif")
     ) dmem_mem (
-        .clock0(clock),
+        // Port A (used)
         .address_a(address),
+        .clock0(clock),
         .data_a(data),
         .wren_a(wren),
-        .q_a(q),
-        .aclr0(1'b0),
-        .aclr1(1'b0),
-        .clock1(1'b0),
-        .address_b(1'b0),
-        .data_b(32'b0),
-        .wren_b(1'b0),
-        .q_b()
+        .q_a(q)
+
+        // No Port B or clock1 signals
     );
 endmodule
--- PRJ4/imem.v
+++ PRJ4/imem.v
@@ -1,7 +1,7 @@
 // You need to generate this component correctly
 module imem (
-    input [11:0] address,
-    input clock,
+    input  [11:0] address,
+    input         clock,
     output [31:0] q
 );
     altsyncram #(
@@ -12,17 +12,15 @@ module imem (
         .outdata_reg_a("UNREGISTERED"),
         .init_file("imem.mif")
     ) imem_mem (
-        .clock0(clock),
+        // Port A (used)
         .address_a(address),
+        .clock0(clock),
         .q_a(q),
+
+        // Tie off unused write ports
         .wren_a(1'b0),
-        .data_a(32'b0),
-        .aclr0(1'b0),
-        .aclr1(1'b0),
-        .clock1(1'b0),
-        .address_b(1'b0),
-        .data_b(32'b0),
-        .wren_b(1'b0),
-        .q_b()
+        .data_a(32'b0)
+
+        // All Port B and clock1 lines are omitted
     );
 endmodule
